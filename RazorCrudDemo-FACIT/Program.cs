9ba8d062f9312c4366695eb4ae2d321cd8e33cb6
Richard Chalk <barmybrit@gmail.com>
2022-12-12 21:51:45 +0100

    Added Dependency Injection for DbContext in program.cs

--- RazorCrudDemo-FACIT/Program.cs
+++ RazorCrudDemo-FACIT/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RazorCrudDemo_FACIT.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Lägg till min DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
