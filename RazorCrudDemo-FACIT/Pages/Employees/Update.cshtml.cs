60e9fbe0cc6a51816cf9d090592d44c2e51f19fb
Richard Chalk <barmybrit@gmail.com>
2022-12-13 15:40:07 +0100

    Updataed OnGet() in Update.cshtml.cs

--- RazorCrudDemo-FACIT/Pages/Employees/Update.cshtml.cs
+++ RazorCrudDemo-FACIT/Pages/Employees/Update.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorCrudDemo_FACIT.Data;
+using RazorCrudDemo_FACIT.Data.Viewmodels;
 
 namespace RazorCrudDemo_FACIT.Pages.Employees
 {
     public class UpdateModel : PageModel
     {
+        [BindProperty]
+        public UpdateEmployeeViewModel UpdateEmployee { get; set; }
+
         private readonly ApplicationDbContext _dbContext;
 
         public UpdateModel(ApplicationDbContext dbContext)
@@ -15,7 +19,19 @@ namespace RazorCrudDemo_FACIT.Pages.Employees
         public void OnGet(Guid id)
         {
             var employee = _dbContext.Employees.Find(id);
+
+            if (employee != null)
+            {
+                UpdateEmployee = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    DateOfBirth = employee.DateOfBirth,
+                    Salary = employee.Salary,
+                    Department = employee.Department
+                };
+            }
         }
     }
-
 }
