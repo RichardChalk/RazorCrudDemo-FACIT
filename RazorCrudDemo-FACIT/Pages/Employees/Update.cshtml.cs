42085b743d9198340c6a4c8ce895c1f789961219
Richard Chalk <barmybrit@gmail.com>
2022-12-13 15:33:25 +0100

    Update.cshtml.cs with dbContext and ID

--- RazorCrudDemo-FACIT/Pages/Employees/Update.cshtml.cs
+++ RazorCrudDemo-FACIT/Pages/Employees/Update.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCrudDemo_FACIT.Data;
 
 namespace RazorCrudDemo_FACIT.Pages.Employees
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _dbContext;
+
+        public UpdateModel(ApplicationDbContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+        public void OnGet(Guid id)
+        {
+            var employee = _dbContext.Employees.Find(id);
         }
     }
+
 }
